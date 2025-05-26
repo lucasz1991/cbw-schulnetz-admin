fd9c614c8fca9a97f0c2e86817d8dfe9965bfa4c
Lucas Zacharias <lucas@zacharias-net.de>
2025-05-26 14:24:30 +0200

    first commit

---
+++ app/Models/ClaimRating.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+use App\Jobs\ClaimRatingAIEval;
+
+
+class ClaimRating extends Model
+{
+    use HasFactory, SoftDeletes;
+    // Possible statuses for the ClaimRating model:
+    // - pending: The claim rating is awaiting review.
+    // - approved: The claim rating has been approved.
+    // - rejected: The claim rating has been rejected.
+    protected $fillable = [
+        'user_id',
+        'insurance_subtype_id',
+        'insurance_type_id',
+        'insurance_id',
+        'rating_questionnaire_versions_id',
+        'answers',
+        'status',
+        'attachments',
+        'rating_score',
+        'moderator_comment',
+        'is_public',
+        'verification_hash',
+    ];
+
+    protected $casts = [
+        'answers' => 'array',
+        'attachments' => 'array',
+        'is_public' => 'boolean',
+    ];
+
+    public function reanalyse(){
+        $this->status = 'rating';
+        $this->saveQuietly();
+        ClaimRatingAIEval::dispatch($this);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function insurance()
+    {
+        return $this->belongsTo(Insurance::class);
+    }
+
+    public function insuranceType()
+    {
+        return $this->belongsTo(InsuranceType::class);
+    }
+    
+    public function insuranceSubtype()
+    {
+        return $this->belongsTo(InsuranceSubtype::class);
+    }
+
+    public function questionnaireVersion()
+    {
+        return $this->belongsTo(RatingQuestionnaireVersion::class);
+    }
+}
---
+++ app/Models/Insurance.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\InsuranceType;
+
+class Insurance extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'style',
+        'initials',
+        'color',
+        'is_active',
+        'order_id',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'is_active' => 'boolean',
+        'style' => 'array',
+        'order_id' => 'integer',
+    ];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+   
+    public function insuranceTypes()
+    {
+        return $this->belongsToMany(InsuranceType::class, 'insurance_insurance_type')
+                    ->withPivot('order_column')
+                    ->orderBy('insurance_insurance_type.order_column');
+    }
+}
