ca49a5bd4d3c83bccbf21e3a1354ec971859b329
Lucas Zacharias <lucas@zacharias-net.de>
2025-11-13 10:35:08 +0100

    fgfg

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,9 +2,9 @@
 <div class="fixed bottom-0 z-10 h-screen ltr:border-r rtl:border-l vertical-menu rtl:right-0 ltr:left-0 top-[70px] pt-12 bg-slate-50 border-gray-50 print:hidden ">
 
     <div data-simplebar class="h-full">
-        <!--- Sidemenu -->
+
         <div class="metismenu pb-10 pt-2.5" id="sidebar-menu">
-            <!-- Left Menu Start -->
+  
             <ul id="side-menu">
                 <li>
                     <a href="{{ url('/') }}" class="block py-2.5 px-6 text-sm font-medium text-gray-600 transition-all duration-150 ease-linear hover:text-blue-500 ">
@@ -94,11 +94,11 @@
                         <li>
                             <a href="" class="pl-[52.8px] pr-6 py-[6.4px] block text-[13.5px]  font-medium text-gray-600 transition-all duration-150 ease-linear hover:text-blue-500 ">
                                 <i data-feather="edit-3" fill="#545a6d33"></i>
-                                <span>Teilnehmer Anfragen</span>
+                                <span>Anfragen</span>
                             </a>
                         </li>
                     </ul>
-                </li>-->
+                </li>
                 
 
                 <li class="px-5 py-3 text-xs font-medium text-gray-500 cursor-default leading-[18px] group-data-[sidebar-size=sm]:hidden block" data-key="t-menu">Dev Tests</li>
@@ -112,7 +112,5 @@
                 @endif
             </ul>
         </div>
-        <!-- Sidebar -->
     </div>
 </div>
-<!-- Left Sidebar End -->
