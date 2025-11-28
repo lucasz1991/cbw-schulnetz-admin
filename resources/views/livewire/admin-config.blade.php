7b4ba2d703b561baab973388c19c0510eba59058
Lucas Zacharias <lucas@zacharias-net.de>
2025-11-28 03:09:00 +0100

    sd

--- resources/views/livewire/admin-config.blade.php
+++ resources/views/livewire/admin-config.blade.php
@@ -25,6 +25,7 @@
                 >
                     Benutzer
                 </button>
+                @if(Auth::user()->role == "admin" )
                 <button 
                     class="whitespace-nowrap pb-4 px-1 border-b-2 font-medium text-sm"
                     :class="{ 'border-blue-500 text-blue-600': activeTab === 'mails', 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300': activeTab !== 'mails' }"
@@ -32,7 +33,6 @@
                 >
                     Mail's
                 </button>
-                @if(Auth::user()->role == "admin" )
                 <button 
                     class="whitespace-nowrap pb-4 px-1 border-b-2 font-medium text-sm"
                     :class="{ 'border-blue-500 text-blue-600': activeTab === 'api', 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300': activeTab !== 'api' }"
