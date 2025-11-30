d21e79dac8bf78d78941479a722101413aeb4903
Lucas Zacharias <lucas@zacharias-net.de>
2025-11-30 20:07:23 +0100

    sdsd

--- resources/views/livewire/admin-config.blade.php
+++ resources/views/livewire/admin-config.blade.php
@@ -25,14 +25,14 @@
                 >
                     Benutzer
                 </button>
-                @if(Auth::user()->role == "admin" )
                 <button 
-                    class="whitespace-nowrap pb-4 px-1 border-b-2 font-medium text-sm"
-                    :class="{ 'border-blue-500 text-blue-600': activeTab === 'mails', 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300': activeTab !== 'mails' }"
-                    @click="activeTab = 'mails'"
+                class="whitespace-nowrap pb-4 px-1 border-b-2 font-medium text-sm"
+                :class="{ 'border-blue-500 text-blue-600': activeTab === 'mails', 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300': activeTab !== 'mails' }"
+                @click="activeTab = 'mails'"
                 >
                     Mail's
                 </button>
+                @if(Auth::user()->role == "admin" )
                 <button 
                     class="whitespace-nowrap pb-4 px-1 border-b-2 font-medium text-sm"
                     :class="{ 'border-blue-500 text-blue-600': activeTab === 'api', 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300': activeTab !== 'api' }"
