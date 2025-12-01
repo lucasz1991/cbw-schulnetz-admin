1bba14423b8124836939b85ccd8c5892dfa6a65f
Lucas Zacharias <lucas@zacharias-net.de>
2025-12-01 17:49:21 +0100

    sdsd

--- resources/views/components/navigation/open-tasks-counter.blade.php
+++ resources/views/components/navigation/open-tasks-counter.blade.php
@@ -3,7 +3,7 @@
 @endphp
 
 @if($count > 0)
-    <span class="ml-2 inline-flex items-center rounded-full bg-red-500 px-2 py-0.5 text-xs font-semibold text-white">
+    <span class="ml-2 inline-flex items-center rounded-full bg-red-500 px-2 py-0.5 text-xs font-semibold text-white"  title="{{ $count }} offene Aufgaben">
         {{ $count }}
     </span>
 @endif
