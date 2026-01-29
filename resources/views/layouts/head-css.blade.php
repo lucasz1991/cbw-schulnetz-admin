c4d96fd2d3a7621d6792b0a2a6722cdf4ee4faac
Lucas Zacharias <lucas@zacharias-net.de>
2026-01-29 15:54:50 +0100

    sdsd

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -4,8 +4,8 @@
 <link rel="stylesheet" href="{{ URL::asset('build/libs/apexcharts/apexcharts.css') }}" />
         <link rel="stylesheet" href="https://unpkg.com/trix@2.0.8/dist/trix.css">
         <script src="https://unpkg.com/trix@2.0.8/dist/trix.umd.min.js" defer></script>
-        <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
-        <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+<script src="https://unpkg.com/dropzone@6.0.0-beta.1/dist/dropzone-min.js"></script>
+    <link href="https://unpkg.com/dropzone@6.0.0-beta.1/dist/dropzone.css" rel="stylesheet" type="text/css" />
         <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js" defer></script>
         
    <link rel="stylesheet" href="{{ asset('adminresources/fontawesome6/css/all.min.css') }}">
