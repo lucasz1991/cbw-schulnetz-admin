c21e21a1c9a8e4bed53c278978013093f60d36ad
Lucas Zacharias <lucas@zacharias-net.de>
2026-01-29 15:26:34 +0100

    ssss

--- app/Livewire/Admin/Assets/OnboardingManagement.php
+++ app/Livewire/Admin/Assets/OnboardingManagement.php
@@ -166,7 +166,7 @@ class OnboardingManagement extends Component
 
             // Create: 1 Datei required; Edit: optional
             'fileUploads' => [$this->editingId ? 'nullable' : 'required', 'array', 'max:1'],
-            'fileUploads.*' => ['file', 'mimes:mp4,wav,pdf', 'max:101200'], // max 99 MB + Overhead
+            'fileUploads.*' => ['file', 'mimes:mp4,wav,pdf'], // max 99 MB + Overhead
         ];
     }
 
