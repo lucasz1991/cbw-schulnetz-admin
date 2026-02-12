dd89dfd64ac2411f844d738b81221f4514ee1c36
Lucas Zacharias <lucas@zacharias-net.de>
2026-02-12 11:54:26 +0100

    asasa

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,8 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('activity:clean-old')
-            ->dailyAt('02:00');
+        // $schedule->command('inspire')->hourly();
     }
 
     /**
