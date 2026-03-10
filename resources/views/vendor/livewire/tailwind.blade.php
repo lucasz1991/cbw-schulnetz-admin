9c28a34d7dc84feaf53cf0771b188ed7d5254070
lucas@zacharias-net.de <lucas@zacharias-net.de>
2026-03-10 02:54:23 +0100

    kjik

--- resources/views/vendor/livewire/tailwind.blade.php
+++ resources/views/vendor/livewire/tailwind.blade.php
@@ -41,7 +41,7 @@ $scrollIntoViewJsSnippet = ($scrollTo !== false)
 
             <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
                 <div>
-                    <p class="inline-flex items-center px-4 py-2 text-sm text-gray-700 bg-white border border-gray-300 rounded-md leading-5">
+                    <p class="inline-flex items-center px-4 py-2 text-sm text-gray-700 bg-white border border-gray-300 rounded-md leading-5 space-x-1">
                         <span>{{ __('pagination.showing') }}</span>
                         <span class="font-medium">{{ $paginator->firstItem() }}</span>
                         <span>{{ __('pagination.to') }}</span>
