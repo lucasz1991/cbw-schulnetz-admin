6705c65812940822b57dc4bd7802067455628176
Lucas Zacharias <lucas@zacharias-net.de>
2026-04-21 19:52:37 +0200

    Remove informational alert from user management interface for a cleaner UI

--- resources/views/livewire/admin/users.blade.php
+++ resources/views/livewire/admin/users.blade.php
@@ -1,27 +1,5 @@
 <div x-data="{ selectedUsers: @entangle('selectedUsers'), search: @entangle('search'), hasUsers: @entangle('hasUsers') }"  wire:loading.class="cursor-wait">
     @persist('scrollbar')
-    <div class="bg-blue-50 border-l-4 border-blue-500 text-blue-700 p-4 mb-6">
-        <div class="flex">
-            <div class="flex-shrink-0">
-                <svg class="h-6 w-6 text-blue-500" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 23.625 23.625" fill="currentColor" aria-hidden="true">
-                    <path
-                        d="M11.812 0C5.289 0 0 5.289 0 11.812s5.289 11.813 11.812 11.813 11.813-5.29 11.813-11.813S18.335 0 11.812 0zm2.459 18.307c-.608.24-1.092.422-1.455.548a3.838 3.838 0 0 1-1.262.189c-.736 0-1.309-.18-1.717-.539s-.611-.814-.611-1.367c0-.215.015-.435.045-.659a8.23 8.23 0 0 1 .147-.759l.761-2.688c.067-.258.125-.503.171-.731.046-.23.068-.441.068-.633 0-.342-.071-.582-.212-.717-.143-.135-.412-.201-.813-.201-.196 0-.398.029-.605.09-.205.063-.383.12-.529.176l.201-.828c.498-.203.975-.377 1.43-.521a4.225 4.225 0 0 1 1.29-.218c.731 0 1.295.178 1.692.53.395.353.594.812.594 1.376 0 .117-.014.323-.041.617a4.129 4.129 0 0 1-.152.811l-.757 2.68a7.582 7.582 0 0 0-.167.736 3.892 3.892 0 0 0-.073.626c0 .356.079.599.239.728.158.129.435.194.827.194.185 0 .392-.033.626-.097.232-.064.4-.121.506-.17l-.203.827zm-.134-10.878a1.807 1.807 0 0 1-1.275.492c-.496 0-.924-.164-1.28-.492a1.57 1.57 0 0 1-.533-1.193c0-.465.18-.865.533-1.196a1.812 1.812 0 0 1 1.28-.497c.497 0 .923.165 1.275.497.353.331.53.731.53 1.196 0 .467-.177.865-.53 1.193z"
-                        data-original="#030104" />
-                </svg>
-            </div>
-            <div class="ml-3">
-                <div class="text-sm">
-                    <p>In der Benutzerverwaltung können Sie folgende Aktionen ausführen:</p>
-                    <ul class="list-disc pl-5">
-                        <li>Benutzer durchsuchen und filtern</li>
-                        <li>Benutzer standardmäßig nach letzter Aktivität (neueste zuerst), Name, E-Mail oder Registrierungsdatum sortieren</li>
-                        <li>Mehrere Benutzer auswählen und Massenaktionen wie Aktivieren, Deaktivieren oder Löschen durchführen</li>
-                        <li>E-Mails an ausgewählte Benutzer senden</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
     <div class="mb-4 flex flex-wrap  justify-between gap-4">
         <div class="mb-6 max-w-md">
             <h1 class="text-2xl font-bold text-gray-800">Benutzer</h1>
