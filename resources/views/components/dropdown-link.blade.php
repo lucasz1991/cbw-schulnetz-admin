fd11639bebc7730a92283f613bf1d7e60bc10288
Lucas Zacharias <lucas@zacharias-net.de>
2026-04-29 22:20:11 +0200

    fix: Correct cursor styling for enabled dropdown links

--- resources/views/components/dropdown-link.blade.php
+++ resources/views/components/dropdown-link.blade.php
@@ -16,6 +16,8 @@ $isDisabled = (bool) $disabled || $isDeniedByCan;
 $classes = 'inline-flex items-center w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out';
 if ($isDisabled) {
     $classes .= ' opacity-80 cursor-not-allowed hover:bg-transparent';
+}else {
+    $classes .= ' cursor-pointer';
 }
 
 $title = $isDeniedByCan
